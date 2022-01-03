ebaee00da3ca8da16b5b77d4628cb38f2b768327
David Ford <dford215@gmail.com>
2022-01-03 15:53:44 -0500

    upload, and read players csv file

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const fs = require('fs');
 const multer = require('multer');
 const upload = multer({ dest: '.' });
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+const players = require('./players.js');
 
 var app = module.exports = express();
 
@@ -21,23 +23,43 @@ var app = module.exports = express();
 //     req.key = key;
 //     next();
 //   });
-app.use('/', express.static(__dirname + '/static'));
-app.use(bodyParser({uploadDir:__dirname, keepExtensions:true}));
-
+app.use('/', express.static(__dirname + '/uploads'));
+// app.use(bodyParser({uploadDir:__dirname, keepExtensions:true}));
+// app.use(multer({dest:__dirname + '/uploads/'}));
+app.use(fileUpload());
 app.get("/api", function(req, res, next){
     res.send("Hello World");
 });
 
-app.post('/api/scores/upload', upload.single('file'), function(req, res, next){
-//app.post('/api/scores/upload', function(req, res, next){
-    console.log(req.body.csv);
-    console.log("Files: " + req.files);
-    console.log("File: " + req.file);
-    let buff = Buffer.from(req.body.csv, 'base64');
-    let text = buff.toString('utf-8');
-    console.log(text);
+//app.post('/api/scores/upload', upload.single('file'), function(req, res, next){
+app.post('/api/scores/upload', function(req, res, next){
+    // console.log("Files: " + req.files);
+    // console.log("File: " + req.file);
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    let csv = req.files.csv;
+
+    // Use the mv() method to place the file somewhere on your server
+    csv.mv(__dirname + '/uploads/' + csv.name, function(err) {
+        if (err)
+        return res.status(500).send(err);
+
+        res.send('File uploaded!');
+    });
+    //console.log(text);
 });
 
-app.post('/api/players/upload', function(req, res, next){});
+app.post('/api/players/upload', function(req, res, next){
+    let csv = req.files.csv;
+
+    // Use the mv() method to place the file somewhere on your server
+    csv.mv(__dirname + '/uploads/' + csv.name, function(err) {
+        if (err)
+        return res.status(500).send(err);
+    });
+
+    players.readPlayersCsv(__dirname + '/uploads/' + csv.name);
+
+    res.send('File uploaded!');
+});
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
