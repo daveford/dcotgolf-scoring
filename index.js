6c23a5f0ddc7c67347b61774d4504cf2be896a72
David Ford <dford215@gmail.com>
2022-01-03 16:06:02 -0500

    fix path in require

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require('express');
 const fileUpload = require('express-fileupload');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-const players = require('./players.js');
+const players = require('./players/players.js');
 
 var app = module.exports = express();
 
