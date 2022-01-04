7546ae814173c7633e78384014579c5604bc5e73
David Ford <dford215@gmail.com>
2022-01-04 16:15:34 -0500

    update roster to Roster

--- players/players.js
+++ players/players.js
@@ -6,21 +6,21 @@ var Roster = require('../models/league-roster');
 
 const Players = class{
     async readPlayersCsv(filepath){
-        let roster = new Roster();
+        //let roster = new Roster();
         fs.createReadStream(path.resolve(filepath))
             .pipe(csv.parse({ headers: false }))
             .on('error', error => console.error(error))
             .on('data', (row) => {
-                if(row[0].toLowerCase() == "season") roster.season(row[1]);
+                if(row[0].toLowerCase() == "season") Roster.season(row[1]);
                 else if(row[0].toLowerCase() == "name" || row[0] == "") ;
                 else {
-                    roster.addPlayer({
+                    Roster.addPlayer({
                         name: row[0],
                         handicap: row[1],
                         isActive: row[2]
                     });
                 }
-                console.log(JSON.stringify(roster));
+                console.log(JSON.stringify(Roster));
             })
             
     }
