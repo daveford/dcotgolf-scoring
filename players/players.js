cbb2657233b46331c9dbfbbafa063e0deb1c89cc
David Ford <dford215@gmail.com>
2022-01-04 16:52:40 -0500

    testing object reference

--- players/players.js
+++ players/players.js
@@ -2,25 +2,26 @@ const fs = require('fs');
 const path = require('path');
 const csv = require('fast-csv');
 const parse = require('csv-parse');
-var Roster = require('../models/league-roster.js');
+const Roster = require('../models/league-roster.js');
 
 const Players = class{
     async readPlayersCsv(filepath){
-        let roster = new Roster();
+        var roster = new Roster();
         fs.createReadStream(path.resolve(filepath))
             .pipe(csv.parse({ headers: false }))
             .on('error', error => console.error(error))
             .on('data', (row) => {
-                if(row[0].toLowerCase() == "season") roster.season(row[1]);
-                else if(row[0].toLowerCase() == "name" || row[0] == "") ;
-                else {
-                    roster.addPlayer({
-                        name: row[0],
-                        handicap: row[1],
-                        isActive: row[2]
-                    });
-                }
-                console.log(JSON.stringify(roster));
+                console.log(roster);
+                // if(row[0].toLowerCase() == "season") roster.season(row[1]);
+                // else if(row[0].toLowerCase() == "name" || row[0] == "") ;
+                // else {
+                //     roster.addPlayer({
+                //         name: row[0],
+                //         handicap: row[1],
+                //         isActive: row[2]
+                //     });
+                // }
+                // console.log(JSON.stringify(roster));
             })
             
     }
