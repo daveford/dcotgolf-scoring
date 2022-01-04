6510ad1e620fc1d5c20d04523624d0f59b22a74f
David Ford <dford215@gmail.com>
2022-01-04 16:54:17 -0500

    update assignment

--- players/players.js
+++ players/players.js
@@ -11,17 +11,17 @@ const Players = class{
             .pipe(csv.parse({ headers: false }))
             .on('error', error => console.error(error))
             .on('data', (row) => {
-                console.log(roster);
-                // if(row[0].toLowerCase() == "season") roster.season(row[1]);
-                // else if(row[0].toLowerCase() == "name" || row[0] == "") ;
-                // else {
-                //     roster.addPlayer({
-                //         name: row[0],
-                //         handicap: row[1],
-                //         isActive: row[2]
-                //     });
-                // }
-                // console.log(JSON.stringify(roster));
+                //console.log(roster);
+                if(row[0].toLowerCase() == "season") roster.season = row[1];
+                else if(row[0].toLowerCase() == "name" || row[0] == "") ;
+                else {
+                    roster.addPlayer({
+                        name: row[0],
+                        handicap: row[1],
+                        isActive: row[2]
+                    });
+                }
+                console.log(JSON.stringify(roster));
             })
             
     }
