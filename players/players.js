984657707635d0631cbd487d88f2616abbd29181
David Ford <dford215@gmail.com>
2022-01-03 20:35:10 -0500

    await file reading

--- players/players.js
+++ players/players.js
@@ -3,7 +3,7 @@ var parse = require('csv-parse');
 
 const Players = class{
     readPlayersCsv(filepath){
-        fs.readFile(filepath, function (err, fileData) {
+        await fs.readFile(filepath, function (err, fileData) {
             parse(fileData, {columns: false, trim: true}, function(err, rows) {
               // Your CSV data is in an array of arrys passed to this callback as rows.
               console.log(fileData);
