e1aefca73844fad31bce402c4cbc1055d29e28db
David Ford <dford215@gmail.com>
2022-01-03 20:46:24 -0500

    test file upload again

--- index.js
+++ index.js
@@ -52,13 +52,13 @@ app.post('/api/players/upload', function(req, res, next){
     let csv = req.files.csv;
 
     // Use the mv() method to place the file somewhere on your server
-    csv.mv(__dirname + '/uploads/' + csv.name, async function(err) {
+    csv.mv(__dirname + '/uploads/' + csv.name, function(err) {
         if (err){
             console.log(err);
             return res.status(500).send(err);
         } 
 
-        await players.readPlayersCsv(__dirname + '/uploads/' + csv.name);
+        //await players.readPlayersCsv(__dirname + '/uploads/' + csv.name);
 
         res.send('File uploaded!');
     });
