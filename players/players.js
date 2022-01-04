46388816f79c180b4b725e05119f5a0f40614d1f
David Ford <dford215@gmail.com>
2022-01-04 15:29:44 -0500

    use self to refernce parent

--- players/players.js
+++ players/players.js
@@ -7,20 +7,21 @@ const Roster = require('../models/league-roster');
 const Players = class{
     async readPlayersCsv(filepath){
         var roster = new Roster();
+        var self = this;
         fs.createReadStream(path.resolve(filepath))
             .pipe(csv.parse({ headers: false }))
             .on('error', error => console.error(error))
             .on('data', (row) => {
-                if(row[0].toLowerCase() == "season") roster.season( row[1]);
+                if(row[0].toLowerCase() == "season") self.roster.season(row[1]);
                 else if(row[0].toLowerCase() == "name" || row[0] == "") ;
                 else {
-                    roster.addPlayer({
+                    self.roster.addPlayer({
                         name: row[0],
                         handicap: row[1],
                         isActive: row[2]
                     });
                 }
-                console.log(JSON.stringify(roster));
+                console.log(JSON.stringify(self.roster));
             })
             
     }
