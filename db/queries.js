5383a9f273a931fe2bbb00ec693bb4bdcb3987cc
David Ford <dford215@gmail.com>
2022-01-05 16:45:45 -0500

    enable ssl in db connection config

--- db/queries.js
+++ db/queries.js
@@ -1,11 +1,12 @@
 const { Client } = require('pg');
-const Pool = require('pg').Pool
+const { Pool } = require('pg');
 const pool = new Pool({
   user: process.env.DATABASE_USERNAME,
   host: process.env.DATABASE_HOST,
   database: process.env.DATABASE_NAME,
   password: process.env.DATABASE_PASSWORD,
   port: 5432,
+  ssl: true
 })
 // var client;
 // class DbManager {
